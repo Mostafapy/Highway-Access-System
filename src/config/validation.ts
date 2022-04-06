0e44d8eb04cb5b55cf699672643c1ea77973116b
mostafa hisham <mostafa.hisham@forus.cc>
2022-04-07 00:24:34 +0200

    add typeorm validations

--- src/config/validation.ts
+++ src/config/validation.ts
@@ -6,4 +6,11 @@ export const validationSchema = Joi.object({
     .default('development'),
   PORT: Joi.number().port().default(3000),
   APP_NAME: Joi.string().required(),
+  TYPEORM_HOST: Joi.string().required(),
+  TYPEORM_PORT: Joi.number().port().required(),
+  TYPEORM_USERNAME: Joi.string().required(),
+  TYPEORM_PASSWORD: Joi.string().required(),
+  TYPEORM_DATABASE: Joi.string().required(),
+  TYPEORM_SYNCHRONIZE: Joi.boolean().optional(),
+  TYPEORM_LOGGING: Joi.string().optional(),
 });
