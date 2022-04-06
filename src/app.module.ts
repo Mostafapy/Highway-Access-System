2a3cadf4d3b1f66c7b130d1852af1a97f9590a93
mostafa hisham <mostafa.hisham@forus.cc>
2022-04-06 20:06:02 +0200

    fFnishing up configuration in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { JoiPipeModule } from 'nestjs-joi';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import configuration from './config/configuration';
+import { validationSchema } from './config/validation';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({
+    isGlobal: true,
+    load: [configuration],
+    validationSchema,
+  }), JoiPipeModule],
   controllers: [AppController],
   providers: [AppService],
 })
