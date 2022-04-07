675b90a5c1b3d2458d4be7fa56925ad5dddaa83e
mostafa hisham <mostafa.hisham@forus.cc>
2022-04-07 13:14:43 +0200

    adding EmployeeService to providers and exports

--- src/employees/employee.module.ts
+++ src/employees/employee.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Employee } from './entities/employee.entity';
+import { EmployeeService } from './services/employees.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Employee])],
   controllers: [],
-  providers: [],
-  exports: [],
+  providers: [EmployeeService],
+  exports: [EmployeeService],
 })
 export class EmployeeModule {}
