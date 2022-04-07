23ee09704675d305b3813e2a43475d2017d9d1f4
mostafa hisham <mostafa.hisham@forus.cc>
2022-04-07 03:32:12 +0200

    add modules imports in AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AccessCardModule } from 'access-cards/access-card.module';
+import { CarModule } from 'cars/car.module';
+import { MYSQL_CONFIG_OPTIONS } from 'config/mysql-configuration';
+import { EmployeeModule } from 'employees/employee.module';
 import { JoiPipeModule } from 'nestjs-joi';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -13,7 +18,13 @@ import { validationSchema } from './config/validation';
       load: [configuration],
       validationSchema,
     }),
+    TypeOrmModule.forRoot({
+      ...MYSQL_CONFIG_OPTIONS,
+    }),
     JoiPipeModule,
+    EmployeeModule,
+    CarModule,
+    AccessCardModule,
   ],
   controllers: [AppController],
   providers: [AppService],
