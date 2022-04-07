e312670995c6ee39384f0e169f7c0d3dd78133d1
mostafa hisham <mostafa.hisham@forus.cc>
2022-04-07 13:12:02 +0200

    adding some options to NestFactory

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,10 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: ['log', 'error', 'warn'],
+    cors: true, // TODO: set allowed origins in production & staging environments
+  });
 
   const config = app.get(ConfigService);
 
