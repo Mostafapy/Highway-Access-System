3ebc595d13f0c7d765f49201027784329a12bfde
mostafa hisham <mostafa.hisham@forus.cc>
2022-04-07 10:34:14 +0200

    adding helper functions for passwords following: - hash passwords - comparePasswords

---
+++ src/auth/helpers/passwords.helper.ts
@@ -0,0 +1,10 @@
+import { compare, hash } from 'bcrypt';
+import { SALT } from '../../config/constants/common.constant';
+
+export const hashPassword = (password: string) => {
+  return hash(password, SALT);
+};
+
+export const comparePasswords = (password: string, hashValue: string): Promise<boolean> => {
+  return compare(password, hashValue);
+};
