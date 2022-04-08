698e5b419dd90a952450b555dba2d77ad1833a83
mostafa hisham <mostafa.hisham@forus.cc>
2022-04-08 15:42:17 +0200

    fix when token is expired

--- src/auth/guards/jwt-auth.guard.ts
+++ src/auth/guards/jwt-auth.guard.ts
@@ -1,11 +1,12 @@
 import { ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
+import { JwtService } from '@nestjs/jwt';
 import { AuthGuard } from '@nestjs/passport';
 
 export const IS_PUBLIC_KEY = 'isPublic';
 @Injectable()
 export class JwtAuthGuard extends AuthGuard('jwt') {
-  constructor(private reflector: Reflector) {
+  constructor(private reflector: Reflector, private readonly jwtService: JwtService) {
     super();
   }
 
@@ -23,6 +24,13 @@ export class JwtAuthGuard extends AuthGuard('jwt') {
   handleRequest(err: Error, user: any, _, context: ExecutionContext) {
     const req = context.switchToHttp().getRequest();
     const url = req.url;
+
+    const token = this.jwtService.decode(req.headers.authorization.split('Bearer ')[1]) as any;
+
+    const expTime = token.exp * 1000;
+
+    if (Date.now() >= expTime) throw new UnauthorizedException();
+
     const last = url.substring(url.lastIndexOf('/') + 1, url.length);
     if (last === 'login' || last === 'signup') {
       return user;
