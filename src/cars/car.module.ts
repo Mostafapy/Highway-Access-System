2a4ca0e3bb00e55af316c8aa775ca03e1c792b86
mostafa hisham <mostafa.hisham@forus.cc>
2022-04-08 23:50:20 +0200

    fix missing imports

--- src/cars/car.module.ts
+++ src/cars/car.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { JwtModule } from '@nestjs/jwt';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Highway } from 'highways/entities/highway.entity';
 import { CarController } from './controllers/car.controller';
 import { AccessCard } from './entities/access-card.entity';
 import { Car } from './entities/car.entity';
@@ -10,7 +11,7 @@ import { CarService } from './services/car.service';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([Car, AccessCard, Gate]),
+    TypeOrmModule.forFeature([Car, AccessCard, Gate, Highway]),
     JwtModule.registerAsync({
       imports: [ConfigModule],
       useFactory: async (configService: ConfigService) => ({
--- src/highways/highway.module.ts
+++ src/highways/highway.module.ts
@@ -1,11 +1,23 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { HighwayController } from './controllers/highway.controller';
 import { Highway } from './entities/highway.entity';
 import { HighwayService } from './services/highway.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Highway])],
+  imports: [
+    TypeOrmModule.forFeature([Highway]),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('jwt.secret'),
+        signOptions: { expiresIn: configService.get<number>('jwt.expiresIn') },
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [HighwayController],
   providers: [HighwayService],
   exports: [HighwayService],
