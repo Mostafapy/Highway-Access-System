912a10df3dd461ad458b5266161717e74cbe97ef
mostafa hisham <mostafa.hisham@forus.cc>
2022-04-08 12:03:42 +0200

    import jwt module

--- src/cars/car.module.ts
+++ src/cars/car.module.ts
@@ -1,4 +1,6 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AccessCard } from 'access-cards/entities/access-card.entity';
 import { CarController } from './controllers/car.controller';
@@ -6,7 +8,17 @@ import { Car } from './entities/car.entity';
 import { CarService } from './services/car.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Car, AccessCard])],
+  imports: [
+    TypeOrmModule.forFeature([Car, AccessCard]),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('jwt.secret'),
+        signOptions: { expiresIn: configService.get<number>('jwt.expiresIn') },
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [CarController],
   providers: [CarService],
   exports: [CarService],
