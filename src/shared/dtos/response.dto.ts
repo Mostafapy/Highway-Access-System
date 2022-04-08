f7903550d3da65f8f015a1d8e88326d8261e9b28
mostafa hisham <mostafa.hisham@forus.cc>
2022-04-08 20:23:26 +0200

    add swagger ApiResponseProperty

--- src/shared/dtos/response.dto.ts
+++ src/shared/dtos/response.dto.ts
@@ -1,6 +1,15 @@
+import { ApiResponseProperty } from '@nestjs/swagger';
+
 export class ResponseDto<T> {
+  @ApiResponseProperty({ type: Number })
   public statusCode: number;
+
+  @ApiResponseProperty({ type: String })
   public message: string;
+
+  @ApiResponseProperty()
   public error?: any = null;
+
+  @ApiResponseProperty()
   public data?: T | any = null;
 }
