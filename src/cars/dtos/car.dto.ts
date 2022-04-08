55c803947bbef6ffa52b13dfac669fa529eb526c
mostafa hisham <mostafa.hisham@forus.cc>
2022-04-08 03:28:30 +0200

    integrate swagger with car dtos for requests

--- src/cars/dtos/car.dto.ts
+++ src/cars/dtos/car.dto.ts
@@ -1,27 +1,34 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Expose } from 'class-transformer';
 import { IsOptional, IsString } from 'class-validator';
 
 export class CreateCarDto {
+  @ApiProperty({ required: true })
   @IsString()
   brand: string;
 
+  @ApiProperty({ required: true })
   @IsString()
   model: string;
 
+  @ApiProperty({ required: true, name: 'plate_number', minLength: 5 })
   @Expose({ name: 'plate_number' })
   @IsString()
   plateNumber: string;
 }
 
 export class UpdateCarDto {
+  @ApiProperty({ required: false })
   @IsOptional()
   @IsString()
   brand?: string;
 
+  @ApiProperty({ required: false })
   @IsOptional()
   @IsString()
   model?: string;
 
+  @ApiProperty({ required: false, name: 'plate_number', minLength: 5 })
   @Expose({ name: 'plate_number' })
   @IsOptional()
   @IsString()
