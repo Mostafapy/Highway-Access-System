113c09d818521e10f71b360b802a2d71a29d36c3
mostafa hisham <mostafa.hisham@forus.cc>
2022-04-08 03:09:10 +0200

    adding swagger controllers

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerCustomOptions, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -9,6 +10,27 @@ async function bootstrap() {
   });
 
   const config = app.get(ConfigService);
+
+  /**
+   * Swagger implementation config
+   */
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Forus API')
+    .setDescription('The Forus Api Documentation')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const customOptions: SwaggerCustomOptions = {
+    swaggerOptions: {
+      persistAuthorization: true, //want to make sure that the authentication token persists after refreshing the page
+    },
+    customSiteTitle: 'Highway Access API', //page tab title
+    customfavIcon: '', // favicon url
+  };
+
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('docs', app, document, customOptions);
 
   await app.listen(config.get('port'));
   console.log(`Running in port ${config.get('port')}`);
