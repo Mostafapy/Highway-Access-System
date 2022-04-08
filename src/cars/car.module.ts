d0c5ff90fcebae0f5e9bf5cb68d5db0cc0756902
mostafa hisham <mostafa.hisham@forus.cc>
2022-04-08 02:41:04 +0200

    add required imports in CarModule

--- src/cars/car.module.ts
+++ src/cars/car.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AccessCard } from 'access-cards/entities/access-card.entity';
 import { Car } from './entities/car.entity';
+import { CarService } from './services/car.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Car])],
+  imports: [TypeOrmModule.forFeature([Car, AccessCard])],
   controllers: [],
-  providers: [],
-  exports: [],
+  providers: [CarService],
+  exports: [CarService],
 })
 export class CarModule {}
