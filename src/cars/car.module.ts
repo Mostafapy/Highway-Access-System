eba636153cd90e3d7d8d3cef46fb9a06217f685e
mostafa hisham <mostafa.hisham@forus.cc>
2022-04-08 10:14:02 +0200

    add CarController to controllers of CarModule

--- src/cars/car.module.ts
+++ src/cars/car.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AccessCard } from 'access-cards/entities/access-card.entity';
+import { CarController } from './controllers/car.controller';
 import { Car } from './entities/car.entity';
 import { CarService } from './services/car.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Car, AccessCard])],
-  controllers: [],
+  controllers: [CarController],
   providers: [CarService],
   exports: [CarService],
 })
