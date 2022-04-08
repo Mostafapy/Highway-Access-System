386db29a1b6e70d5fd115afd82be0cd508d45dd1
mostafa hisham <mostafa.hisham@forus.cc>
2022-04-08 16:27:43 +0200

    fix all controllers response based on response dto

--- src/auth/controllers/auth.controller.ts
+++ src/auth/controllers/auth.controller.ts
@@ -4,23 +4,35 @@ import { SignupDto } from 'auth/dtos/auth.dto';
 import { LocalAuthGuard } from 'auth/guards';
 import { AuthService } from 'auth/services/auth.service';
 import { Employee } from 'employees/entities/employee.entity';
+import { ResponseDto } from 'shared/dtos/response.dto';
 
 @Controller('v1/auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('/signup')
-  signup(@Body() body: SignupDto): Promise<Employee> {
-    return this.authService.signUp(body);
+  async signup(@Body() body: SignupDto): Promise<ResponseDto<Employee>> {
+    const data = await this.authService.signUp(body);
+
+    return {
+      statusCode: 201,
+      message: 'Successfully Signed Up',
+      data,
+    };
   }
 
   @Post('/login')
   @UseGuards(LocalAuthGuard)
-  async login(@AuthUser() employee: Employee) {
+  async login(@AuthUser() employee: Employee): Promise<ResponseDto<any>> {
     const token = await this.authService.login(employee);
+
     return {
-      ...employee.toJSON(),
-      ...token,
+      statusCode: 200,
+      message: 'Successfully Logged In',
+      data: {
+        ...employee.toJSON(),
+        ...token,
+      },
     };
   }
 }
--- src/cars/controllers/car.controller.ts
+++ src/cars/controllers/car.controller.ts
@@ -1,9 +1,11 @@
 import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
 import { AuthUser } from 'auth/decorators/auth-user.decorator';
 import { CreateCarDto, UpdateCarDto } from 'cars/dtos/car.dto';
+import { Car } from 'cars/entities/car.entity';
 import { CarService } from 'cars/services/car.service';
 import { Employee } from 'employees/entities/employee.entity';
 import { AuthProtect } from 'shared/decorators/auth-protect.decorator';
+import { ResponseDto } from 'shared/dtos/response.dto';
 
 @Controller('v1/cars')
 export class CarController {
@@ -11,35 +13,70 @@ export class CarController {
 
   @AuthProtect()
   @Post('')
-  create(@AuthUser() user: Employee, @Body() body: CreateCarDto) {
-    return this.carService.create(user, body);
+  async create(@AuthUser() user: Employee, @Body() body: CreateCarDto): Promise<ResponseDto<Car>> {
+    const data = await this.carService.create(user, body);
+
+    return {
+      statusCode: 201,
+      message: 'Created Sucessfully',
+      data,
+    };
   }
 
   @AuthProtect()
   @Get('')
-  findAll(@AuthUser() user: Employee) {
-    return this.carService.findAll(user);
+  async findAll(@AuthUser() user: Employee): Promise<ResponseDto<Car[]>> {
+    const data = await this.carService.findAll(user);
+
+    return {
+      statusCode: 200,
+      message: 'All Car Are Retrieved Succesfully',
+      data,
+    };
   }
 
   @AuthProtect()
   @Get(':criterial')
-  findOne(@AuthUser() user: Employee, @Param('criteria') criteria: string) {
-    return this.carService.findOne(user, criteria);
+  async findOne(
+    @AuthUser() user: Employee,
+    @Param('criteria') criteria: string,
+  ): Promise<ResponseDto<Car>> {
+    const data = await this.carService.findOne(user, criteria);
+
+    return {
+      statusCode: 200,
+      message: 'Your Requested Car is Retrieved Succesfully',
+      data,
+    };
   }
 
   @AuthProtect()
   @Put(':criteria')
-  update(
+  async update(
     @AuthUser() user: Employee,
     @Param('criteria') criteria: string,
     @Body() updateCarDto: UpdateCarDto,
-  ) {
-    return this.carService.update(user, criteria, updateCarDto);
+  ): Promise<ResponseDto<Car>> {
+    const data = await this.carService.update(user, criteria, updateCarDto);
+
+    return {
+      statusCode: 200,
+      message: 'Your Requested Car is Updated Succesfully',
+      data,
+    };
   }
 
   @AuthProtect()
   @Delete(':criteria')
-  remove(@AuthUser() user: Employee, @Param('criteria') criteria: string) {
-    return this.carService.delete(user, criteria);
+  async remove(
+    @AuthUser() user: Employee,
+    @Param('criteria') criteria: string,
+  ): Promise<ResponseDto<any>> {
+    await this.carService.delete(user, criteria);
+
+    return {
+      statusCode: 200,
+      message: 'Your Requested Car is Deleted Succesfully',
+    };
   }
 }
