8549edabe8c013cbdde06a116204fb28cec6adf9
mostafa hisham <mostafa.hisham@forus.cc>
2022-04-08 22:37:49 +0200

    add HighwayController to HighwayModule

--- src/highways/highway.module.ts
+++ src/highways/highway.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { HighwayController } from './controllers/highway.controller';
 import { Highway } from './entities/highway.entity';
 import { HighwayService } from './services/highway.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Highway])],
-  controllers: [],
+  controllers: [HighwayController],
   providers: [HighwayService],
   exports: [HighwayService],
 })
